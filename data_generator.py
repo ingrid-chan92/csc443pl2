eca5c3171e3f6d86430d40fc5afbb86e8cbd4666
Hong Xuan Yi <c1hongxu@cdf.toronto.edu>
2014-10-22 11:18:55 -0400

    finished data generator

--- data_generator.py
+++ data_generator.py
@@ -1,3 +1,7 @@
+import json
+import sys
+import random
+
 '''
 You should implement this script to generate test data for your
 merge sort program.
@@ -24,6 +28,35 @@ def generate_data(schema, out_file, nrecords):
   character at the end of every record.
   '''
   print "Generating %d records" % nrecords
+  #name:, length: type: distribution:
+
+  letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+  with open(out_file, 'w') as f:
+    for _ in xrange(nrecords):
+        row = []
+        
+        for entry in range(len(schema)):    
+          if schema[entry]['type'] == 'string':
+            row.append(''.join([random.choice(letters) for j in range(schema[entry]['length'])]))
+            
+          elif (schema[entry]['type'] == 'integer'):
+            dist = schema[entry]['distribution']
+            row.append("%d" % random.randint(dist['min'], dist['max']))
+            
+          elif (schema[entry]['type'] == 'float'):
+            dist = schema[entry]['distribution']
+            val = random.gauss(dist['mu'], dist['sigma'])
+            while (val < dist['min'] or val > dist['max']):
+              val = random.gauss(dist['mu'], dist['sigma'])
+            row.append( "%.2f" % val)
+
+        print >>f, ",".join(row)
+
+  print "Generated %d tuples" % (nrecords)
+
+
+  
 
 if __name__ == '__main__':
   import sys, json
@@ -34,7 +67,6 @@ if __name__ == '__main__':
   schema = json.load(open(sys.argv[1]))
   output = sys.argv[2]
   nrecords = int(sys.argv[3])
-  print schema
-  
-  generate_data(schema, output, nrecords)
 
+  generate_data(schema, output, nrecords)
+  
